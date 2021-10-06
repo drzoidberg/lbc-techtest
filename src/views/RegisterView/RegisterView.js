05ff0f47e47fe3a422791d710e414d07a30e404f
Jorge Vicens Valor <gromit.jvv@gmail.com>
2021-10-06 17:04:07 +0200

    added RegisterView component

---
+++ src/views/RegisterView/RegisterView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ProgressTracker from '../../components/ProgressTracker/ProgressTracker';
+
+const RegisterView = () => {
+  return (
+    <>
+      <div></div>
+      <ProgressTracker />
+    </>
+  );
+};
+
+export default RegisterView;
